10ef34b7806be33c6118f4de27ed633169478948
perfree <perfree@126.com>
2021-12-24 13:26:25 +0800

    first commit

---
+++ src/main/java/com/copy/CopyHandle.java
@@ -0,0 +1,29 @@
+package com.copy;
+
+import cn.hutool.core.util.StrUtil;
+import com.perfree.commons.OptionCacheUtil;
+import com.perfree.plugin.proxy.HtmlRenderProxy;
+import org.jsoup.nodes.Document;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CopyHandle extends HtmlRenderProxy {
+
+    @Override
+    public Document editFrontDocument(Document document, HttpServletResponse response, HttpServletRequest request) {
+        String js = "<script>document.addEventListener('copy',function(e){\n" +
+                "                let clipboardData = e.clipboardData || window.clipboardData;\n" +
+                "                if(!clipboardData) return ;\n" +
+                "                let text = window.getSelection().toString();\n" +
+                "                if(text){\n" +
+                "                    e.preventDefault();\n" +
+                "                    clipboardData.setData('text/plain', text + '\\r\\n{}')\n" +
+                "                }\n" +
+                "            })</script>";
+        String text = OptionCacheUtil.getDefaultValue("PERFREE_COPY_TEXT", "");
+        js = StrUtil.format(js,text);
+        document.body().append(js);
+        return document;
+    }
+}
